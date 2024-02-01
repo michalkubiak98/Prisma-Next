370601a0a3a2c9b8343adc64d0ea5cfda66d0c23
Michal Kubiak <michal.kubiak@globalreachtech.com>
2024-02-01 10:34:46 +0000

    prisma jobs display from db in a json format, env gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-interface FormData {}
+import prisma from '@/lib/prisma';
 
-export default function Home() {
+export default async function Home() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    orderBy: { createdAt: 'desc' },
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hello</h1>
+      {JSON.stringify(jobs)}
     </main>
   );
 }
